332bb9131814b3ef677cb59bdbbeb4f751a4b6b7
okba allaoua <o.allaoua@esi-sba.com>
2022-12-13 00:56:33 +0100

    quests.json edit questions

--- script/script.js
+++ script/script.js
@@ -19,7 +19,7 @@ function reloading(value) {
             location.reload();
             break;
         case 2:
-            window.open("https://github.com/ELHart05", "_self");
+            window.open("https://aws.amazon.com/", "_self");
             break;
     }
 }
